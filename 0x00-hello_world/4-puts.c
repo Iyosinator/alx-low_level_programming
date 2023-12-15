2ae960ec394d3a454f873674e07d6d0ce00976de
iyosinator <iyosiasmulugeta@gmail.com>
2023-12-15 01:06:13 -0800

    Puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+* main - Writing a Program that prints using puts() '"Programming is like building a multilingual puzzle'
+* Return 0 - Always Sucess 
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
