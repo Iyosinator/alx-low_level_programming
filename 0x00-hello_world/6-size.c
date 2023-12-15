7ba5004b5d1613d138515b001915022433611cfc
iyosinator <iyosiasmulugeta@gmail.com>
2023-12-15 05:23:38 -0800

    I think I finish

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- *  main - Enty point
+ *  main - Entry point that tells me the size of data types
  *
  *  Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char acharacter;
-	int ainteger;
-	long along;
-	long long alonglong;
-	float afloat;
+	char mychar;
+	int myint;
+	long mylong;
+	long long mylonglong;
+	float myfloat;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of a char: %lu byte(s)\n", sizeof(mychar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(myint));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(mylong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(mylonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(myfloat));
 
 	return (0);
 
